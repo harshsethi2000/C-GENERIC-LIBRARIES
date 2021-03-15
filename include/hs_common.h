ef9c34ca1ebd3fda291f08b39c3393f041935347
harshsethi2000 <57841517+harshsethi2000@users.noreply.github.com>
2021-03-15 06:33:33 +0530

    Create hs_common.h

---
+++ include/hs_common.h
@@ -0,0 +1,11 @@
+#ifndef __HS_COMMON__H
+#define __HS_COMMON_H
+#define TRUE 1
+#define True 1
+#define true 1
+#define FALSE 0
+#define False 0
+#define false 0
+#define bool int
+#define boolean int
+#endif
